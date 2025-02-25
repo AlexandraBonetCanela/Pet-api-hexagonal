b25ab220c2e848d31df97458b0cf3fd97d727f22
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-02-25 07:44:13 +0100

    cleaned AuthResponse from getters, settters and builder

--- pet/src/main/java/edu/alexandra/pet/application/rest/response/AuthResponse.java
+++ pet/src/main/java/edu/alexandra/pet/application/rest/response/AuthResponse.java
@@ -1,13 +1,7 @@
 package edu.alexandra.pet.application.rest.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@Builder
 @AllArgsConstructor
 public final class AuthResponse {
 
