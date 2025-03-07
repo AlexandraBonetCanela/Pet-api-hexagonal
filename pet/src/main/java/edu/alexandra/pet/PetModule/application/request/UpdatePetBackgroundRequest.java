cf3cc2109e942afe42e7c19be81be46a9713c43c
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-03-07 09:27:28 +0100

    made request class final

--- pet/src/main/java/edu/alexandra/pet/PetModule/application/request/UpdatePetBackgroundRequest.java
+++ pet/src/main/java/edu/alexandra/pet/PetModule/application/request/UpdatePetBackgroundRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class UpdatePetBackgroundRequest {
+public final class UpdatePetBackgroundRequest {
 
     @NotBlank(message = "New Background is required")
     private final String newPetBackground;
